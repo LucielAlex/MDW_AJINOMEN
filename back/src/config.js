e64dddbfb6d07f58613588e9b540ce9546796b28
LucielAlex <kanira2006@gmail.com>
2024-12-07 14:55:01 -0500

    Update config.js

--- back/src/config.js
+++ back/src/config.js
@@ -3,4 +3,4 @@ export const MONGODB_URI =
   process.env.MONGODB_URI || "mongodb+srv://karla:RNQ8zXkhUcSQM5rP@cluster0.jhch9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
 
-export const FRONTEND_URL = process.env.FRONTEND_URL || "https://mdw-front.onrender.com";
+export const FRONTEND_URL = process.env.FRONTEND_URL || "https://mdw-front.onrender.com/api";
